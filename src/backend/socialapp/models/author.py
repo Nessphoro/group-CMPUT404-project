9b26d90a8f69447a713c3776fec402a57b114cba
Shouyang Zhou <s.zhou1511@gmail.com>
2019-02-19 17:56:18 -0700

    - Added __str__ ( or __str__ placeholder) methods to a couple models

--- src/backend/socialapp/models/author.py
+++ src/backend/socialapp/models/author.py
@@ -10,3 +10,6 @@ class Author(models.Model):
     host = models.URLField()
     localuser = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     friends = models.ManyToManyField("Author", blank=True, null=True, related_name="reverse_friends")
+
+    def __str__(self):
+        return "Author({},{},{})".format(self.displayName, self.localuser, self.github)
