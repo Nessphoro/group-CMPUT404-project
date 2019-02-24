21d80868393c7fe83cbf173b739ebd45d7f8e7af
Student <ldmcdona@ualberta.ca>
2019-02-23 23:52:54 -0500

    I learned a science

--- src/backend/socialapp/views/index.py
+++ src/backend/socialapp/views/index.py
@@ -4,5 +4,9 @@ class Index(TemplateView):
     template_engine = 'jinja2'
     template_name = 'socialapp/socialapp_base.html'
 
+class Test(TemplateView):
+    template_engine = 'jinja2'
+    template_name = 'socialapp/Author.html'
+
     def get_context_data(self, **kwargs):
         pass
