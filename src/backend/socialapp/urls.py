f2e2fa8e05181d32219b5f3018b7257565cf754e
ldmcdona <ldmcdona@ualberta.ca>
2019-02-25 10:29:36 -0700

    need to test this later

--- src/backend/socialapp/urls.py
+++ src/backend/socialapp/urls.py
@@ -14,5 +14,6 @@ api_router.register(r'User', views.UserViewSet)
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path('Author/', views.Index, name'Test'), #broken
     path('api/',include(api_router.urls))
 ]
