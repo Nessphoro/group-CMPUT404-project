d55a03052209663777cfe8b1a028546035af92f9
Pavlo Malynin <pavlo@malynin.ca>
2019-03-14 22:33:53 -0600

    Mixins

--- src/backend/socialapp/views/mixin.py
+++ src/backend/socialapp/views/mixin.py
@@ -3,9 +3,9 @@ class MixinContext(object):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         if self.request.user.is_authenticated:
-            Auth = models.Author.objects.filter(localuser=self.request.user)
-            if Auth:
-                context['ActiveUser'] = Auth[0]
+            # Auth = models.Author.objects.filter(localuser=self.request.user)
+            # if Auth:
+            context['ActiveUser'] = self.request.user.author
         return context
 
 
@@ -14,10 +14,10 @@ class MixinIndex(object):
         context = super().get_context_data(**kwargs)
         # get author image
         if self.request.user.is_authenticated:
-            Auth = models.Author.objects.filter(localuser=self.request.user)
-            if Auth:
-                context['ActiveUser'] = Auth[0]
-                context['Posts'] = self.logged_user(Auth[0])
+            # Auth = models.Author.objects.filter(localuser=self.request.user)
+            # if Auth:
+            context['ActiveUser'] = self.request.user.author
+            context['Posts'] = self.logged_user(self.request.user.author)
         else:
             context['Posts'] = self.public_user()
 
--- src/backend/socialapp/views/postUpdateView.py
+++ src/backend/socialapp/views/postUpdateView.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.mixins import UserPassesTestMixin
 import json
 from .. import models
 import pytz
@@ -8,7 +9,7 @@ from datetime import datetime
 from django.urls import reverse_lazy
 from .mixin import MixinContext
 
-class PostUpdateView(MixinContext,UpdateView):
+class PostUpdateView(UserPassesTestMixin, MixinContext, UpdateView):
     template_engine = 'jinja2'
     template_name = 'socialapp/post-update.html'
 
@@ -17,3 +18,9 @@ class PostUpdateView(MixinContext,UpdateView):
 
     def get_success_url(self):
         return reverse_lazy("post-id", kwargs={'pk': self.get_object().id})
+
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+
+        return self.get_object().author == self.request.user.author
