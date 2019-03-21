2d2bfe5d3a7e036968856821b563eeda5fdf0306
Pavlo Malynin <pavlo@malynin.ca>
2019-03-20 20:50:52 -0600

    Add Site URL

--- src/backend/socialapp/pipeline.py
+++ src/backend/socialapp/pipeline.py
@@ -6,6 +6,6 @@ def save_profile(backend, user, response, *args, **kwargs):
     print(backend)
     if backend.name == 'github':
         if not Author.objects.filter(localuser=user):
-            Author.objects.create(github=response['url'],localuser=user ,displayName=response['login'] ,image=response['avatar_url'] , feed=response['events_url'].replace("{/privacy}",""),host=settings.SITE_URL)
+            Author.objects.create(github=response['url'],localuser=user ,displayName=response['login'] ,image=response['avatar_url'] , feed=response['events_url'].replace("{/privacy}",""), host=settings.SITE_URL)
 
 
