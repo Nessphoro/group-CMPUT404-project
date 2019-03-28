f4c253a5adf1eccb215dc542b2a985de71a6ab32
Pavlo Malynin <pavlo@malynin.ca>
2019-03-28 17:32:36 -0600

    Fix up server auth

--- src/backend/socialapp/models/author.py
+++ src/backend/socialapp/models/author.py
@@ -226,6 +226,14 @@ class Author(models.Model):
             if node.host == self.host:
                 return node
 
+    def compute_full_id(self):
+        node = self.get_node()
+
+        if node:
+            return f"{node.endpoint}/author/{self.id}"
+        else:
+            return reverse("api-author", kwargs={'pk': self.id})
+
     def is_foreign_author(self):
         return not (str(self.host) == settings.SITE_URL)
 
