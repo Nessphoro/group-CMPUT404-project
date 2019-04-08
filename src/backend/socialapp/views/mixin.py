c314f0066c3926b8d279451040958cb8717007f1
Pavlo Malynin <pavlo@malynin.ca>
2019-04-08 13:22:28 -0600

    Fetch 100 posts

--- src/backend/socialapp/views/mixin.py
+++ src/backend/socialapp/views/mixin.py
@@ -86,7 +86,7 @@ class MixinIndex(object):
             asyncio.set_event_loop(loop)
             loop.run_until_complete(self.refresh_async(author))
             loop.close()
-            context['Posts'] = author.get_all_posts()[:50]
+            context['Posts'] = author.get_all_posts()[:100]
         else:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
